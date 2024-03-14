b1c7ffa823068ae62b9a7b2fe5a7620f2294e05c
SMN_LCS <simon.lucas.83136@gmail.com>
2024-03-14 10:43:47 +0100

    SITE version sans rien
    juste la navbar avec CSS

---
+++ Index.php
@@ -0,0 +1,57 @@
+
+<html>
+    <head>
+        <title>Le Super Coin</title>
+        <meta charset="UTF-8">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <link href="VPC_css.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="header">
+            <div class="logo">
+                <img class="logoLSC" src="lesupercoin_carré" href="#"></img>
+            </div>  
+            <nav>
+                <form class="search" action="search.php"> 
+                <input name="q" placeholder="Search..." type="search">
+                </form>
+                <ul>
+                <li>
+                    <a href="">Acceuil</a>
+                </li>
+                <li class="dropdown">
+                    <a href="">Boutique</a>
+                    <ul>
+                        <li><a href="#">Enfant</a></li>
+                        <li><a href="#">Femme</a></li>
+                        <li><a href="#">Homme</a></li>
+                        <li><a href="#">Tous</a></li>
+                    </ul>        
+                </li>
+                <li class="dropdown">
+                    <a href="">Compte</a>
+                    <ul>
+                        <li><a href="#">Informations</a></li>
+                        <li><a href="#">Paramètres</a></li>
+                        <li><a href="#">Vos commandes</a></li>
+                        <li><a href="#">Se déconnecter</a></li>
+                    </ul>        
+                </li>
+                <button class="btn_connection">
+                    Connection / Inscription
+                </button>
+                </ul>
+            </nav>
+        </div>
+    </body>
+    
+</html>
+
+<script>
+    $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+	
+    $("#menu-icon").on("click", function(){
+        $("nav").slideToggle();
+        $(this).toggleClass("active");
+    });
+</script>
